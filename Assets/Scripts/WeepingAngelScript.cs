b4a74cb462ef96988d4b841aaafa486d7f3b2c1a
Dalton <n7dalton@gmail.com>
2023-08-16 10:51:31 -0500

    Death.

--- Assets/Scripts/WeepingAngelScript.cs
+++ Assets/Scripts/WeepingAngelScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class WeepingAngelScript : MonoBehaviour
 {
@@ -13,7 +14,7 @@ public class WeepingAngelScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -25,23 +26,28 @@ public class WeepingAngelScript : MonoBehaviour
             Debug.Log("Object is visible");
 
         }
-        else {
+        else
+        {
             Debug.Log("Object is no longer visible");
-                Enemy.transform.LookAt(player);
-            Enemy.transform.Translate(0, 0, 5 * Time.deltaTime); 
+            Enemy.transform.LookAt(player);
+            Enemy.transform.Translate(0, 0, 5 * Time.deltaTime);
         }
     }
     private void OnTriggerEnter(Collider other)
     {
-        if (gameObject.CompareTag("Enemy"))
+        
+        SceneManager.LoadScene("Death.");
+       
+       
+    }
+    private void OnTriggerExit(Collider eyeCollider)
+    {
+        if (gameObject.CompareTag("Player"))
         {
-            lEye.SetActive(true);
-            rEye.SetActive(true);
-        }
-        else
-        {
+            
             lEye.SetActive(false);
             rEye.SetActive(false);
         }
+
     }
 }
